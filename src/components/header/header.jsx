9332dd8b69c78c7396466abf771994ec7321b7a1
Daniyar Otynshin <daniyar.otynshin@yandex.kz>
2021-04-17 03:53:28 +0600

    feat: add Header component

---
+++ src/components/header/header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header className="page__header header">
+            <div className="header__menu menu">
+                <div className="menu__body"></div>
+            </div>
+            <div className="header__logo">
+                media monk
+            </div>
+
+        </header>
+    )
+}
+
+export default Header;
