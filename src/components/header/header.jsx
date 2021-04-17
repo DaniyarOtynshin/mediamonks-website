735e9f97f498d3157ddec15402bcd3166566785b
Daniyar Otynshin <daniyar.otynshin@yandex.kz>
2021-04-17 17:23:20 +0600

    feat: complete html and css for Header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,10 +4,18 @@ function Header() {
     return (
         <header className="page__header header">
             <div className="header__menu menu">
-                <div className="menu__body"></div>
+                <div className="menu__body">
+                    <ul className="menu__list">
+                        <li><a href="#" className="menu__link">about</a></li>
+                        <li><a href="#" className="menu__link">careers</a></li>
+                        <li><a href="#" className="menu__link">contact</a></li>
+                        <li><a href="#" className="menu__link">blog</a></li>
+                        <li><a href="#" className="menu__link">watch our mixtape</a></li>
+                    </ul>
+                </div>
             </div>
             <div className="header__logo">
-                media monk
+                media monks
             </div>
 
         </header>
