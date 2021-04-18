ba1da00f3bc86610eed4e3977435a01c6a1b25c5
Daniyar Otynshin <daniyar.otynshin@yandex.kz>
2021-04-19 04:12:21 +0600

    Add footer styles

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,29 @@ import React from 'react';
 
 function Footer() {
     return (
-        
+        <footer className="page__footer">
+            <div className="table">
+                <div className="table__titles">
+                    <div className="table__title">office time</div>
+                    <div className="table__title">office open</div>
+                    <div className="table__title">addres</div>
+                    <div className="table__title">contact</div>
+                </div>
+                <div className="table__time">23:05</div>
+                <div className="table__time">23:05</div>
+                <div className="table__time">23:05</div>
+                <div className="table__office">Astana</div>
+                <div className="table__office">Kostanay</div>
+                <div className="table__office">London</div>
+                <div className="table__address">Manasa 1</div>
+                <div className="table__address">Manasa 1</div>
+                <div className="table__address">Manasa 1</div>
+                <div className="table__address">Manasa 1</div>
+                <div className="table__contact">1123</div>
+                <div className="table__contact">222</div>
+                <div className="table__contact">42352</div>
+            </div>
+        </footer>
     )
 }
 
