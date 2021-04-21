19f4dbdf66f49890be3bb1cc656800ec278ddef6
Daniyar Otynshin <daniyar.otynshin@yandex.kz>
2021-04-22 02:45:46 +0600

    Change footer flexbox styles

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,27 +4,87 @@ function Footer() {
     return (
         <footer className="page__footer">
             <div className="table">
-                <div className="table__time-column">
-                    <div className="table__title">office time</div>
-                    <div className="table__time">23:05</div>
-                    <div className="table__time">23:05</div>
-                    <div className="table__time">23:05</div>
+                <div className="table__row">
+                    <div className="table__title table__title_time">office time</div>
+                    <div className="table__title table__title_office">office open</div>
+                    <div className="table__title table__title_address">addres</div>
+                    <div className="table__title table__title_address">contact</div>
                 </div>
-                <div className="table__office-column">
-                    <div className="table__title">office open</div>
+                <div className="table__row">
+                    <div className="table__time">23:05</div>
                     <div className="table__office">Astana</div>
-                    <div className="table__office">Kostanay</div>
-                    <div className="table__office">London</div>
-                </div>
-                <div className="table__address-column">
-                    <div className="table__title">addres</div>
-                    <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
                     <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
                     <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
                 </div>
-                <div className="table__address-column">
-                    <div className="table__title">contact</div>
+                <div className="table__row">
+                    <div className="table__time">23:05</div>
+                    <div className="table__office">Astana</div>
                     <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
+                    <div className="table__contact">NY 10017 United States of America</div>
+                </div>
+                <div className="table__row">
+                    <div className="table__time">23:05</div>
+                    <div className="table__office">Nur-Sultan Sultan</div>
+                    <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America 228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
+                    <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
+                </div>
+                <div className="table__row">
+                    <div className="table__time">23:05</div>
+                    <div className="table__office">Astana</div>
+                    <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
+                    <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
+                </div>
+                <div className="table__row">
+                    <div className="table__time">23:05</div>
+                    <div className="table__office">Astana</div>
+                    <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
+                    <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
+                </div>
+                <div className="table__row">
+                    <div className="table__time">23:05</div>
+                    <div className="table__office">Astana</div>
+                    <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
+                    <div className="table__contact">NY 10017 United States of America</div>
+                </div>
+                <div className="table__row">
+                    <div className="table__time">23:05</div>
+                    <div className="table__office">Nur-Sultan Sultan</div>
+                    <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America 228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
+                    <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
+                </div>
+                <div className="table__row">
+                    <div className="table__time">23:05</div>
+                    <div className="table__office">Astana</div>
+                    <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
+                    <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
+                </div>
+                <div className="table__row">
+                    <div className="table__time">23:05</div>
+                    <div className="table__office">Astana</div>
+                    <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
+                    <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
+                </div>
+                <div className="table__row">
+                    <div className="table__time">23:05</div>
+                    <div className="table__office">Astana</div>
+                    <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
+                    <div className="table__contact">NY 10017 United States of America</div>
+                </div>
+                <div className="table__row">
+                    <div className="table__time">23:05</div>
+                    <div className="table__office">Nur-Sultan Sultan</div>
+                    <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America 228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
+                    <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
+                </div>
+                <div className="table__row">
+                    <div className="table__time">23:05</div>
+                    <div className="table__office">Astana</div>
+                    <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
+                    <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
+                </div>
+                <div className="table__row">
+                    <div className="table__time">23:05</div>
+                    <div className="table__office">Astana</div>
                     <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
                     <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
                 </div>
