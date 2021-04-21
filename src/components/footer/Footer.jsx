3b058de85e3b9af1c64e7683ccf2e436d7c96da7
Daniyar Otynshin <daniyar.otynshin@yandex.kz>
2021-04-22 00:57:19 +0600

    Change table styles to flex

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,25 +4,30 @@ function Footer() {
     return (
         <footer className="page__footer">
             <div className="table">
-                <div className="table__titles">
+                <div className="table__time-column">
                     <div className="table__title">office time</div>
-                    <div className="table__title">office open</div>
-                    <div className="table__title">addres</div>
-                    <div className="table__title">contact</div>
+                    <div className="table__time">23:05</div>
+                    <div className="table__time">23:05</div>
+                    <div className="table__time">23:05</div>
                 </div>
-                <div className="table__time">23:05</div>
-                <div className="table__time">23:05</div>
-                <div className="table__time">23:05</div>
-                <div className="table__office">Astana</div>
-                <div className="table__office">Kostanay</div>
-                <div className="table__office">London</div>
-                <div className="table__address">Manasa 1</div>
-                <div className="table__address">Manasa 1</div>
-                <div className="table__address">Manasa 1</div>
-                <div className="table__address">Manasa 1</div>
-                <div className="table__contact">1123</div>
-                <div className="table__contact">222</div>
-                <div className="table__contact">42352</div>
+                <div className="table__office-column">
+                    <div className="table__title">office open</div>
+                    <div className="table__office">Astana</div>
+                    <div className="table__office">Kostanay</div>
+                    <div className="table__office">London</div>
+                </div>
+                <div className="table__address-column">
+                    <div className="table__title">addres</div>
+                    <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
+                    <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
+                    <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
+                </div>
+                <div className="table__address-column">
+                    <div className="table__title">contact</div>
+                    <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
+                    <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
+                    <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
+                </div>
             </div>
         </footer>
     )
