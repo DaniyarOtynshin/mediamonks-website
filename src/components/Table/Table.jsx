e59c6531b938fc3ddc2a17c4eae5a6bf74bb4f62
Daniyar Otynshin <daniyar.otynshin@yandex.kz>
2021-05-01 04:47:48 +0600

    Add cross line on Table row

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -2,86 +2,90 @@ const Table = () => {
     return (
         <div className="page__table table">
                 <div className="table__row">
-                    <div className="table__title table__title_time">office time</div>
-                    <div className="table__title table__title_office">office open</div>
+                    <div className="table__office">
+                        <div className="table__title table__title_time">office time</div>
+                        <div className="table__title table__title_office">office open</div>
+                    </div>
                     <div className="table__title table__title_address">addres</div>
                     <div className="table__title table__title_address">contact</div>
                 </div>
                 <div className="table__row">
-                    <div className="table__time">23:05</div>
-                    <div className="table__office">hilversum</div>
+                    <div className="table__office">
+                        <div className="table__time">23:05</div>
+                        <div className="table__open">hilversum</div>
+                    </div>
                     <div className="table__contact">Oude Amersfoortseweg 125 1212AA Hilversum The Netherlands</div>
                     <div className="table__contact">hilversum@mediamonks.com +31 35 621 21 50</div>
                 </div>
                 <div className="table__row">
-                    <div className="table__time">23:05</div>
-                    <div className="table__office">Astana</div>
+                    <div className="table__office">
+                        <div className="table__time">23:05</div>
+                        <div className="table__open">hilversum</div>
+                    </div>
                     <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
                     <div className="table__contact">NY 10017 United States of America</div>
                 </div>
                 <div className="table__row">
-                    <div className="table__time">23:05</div>
-                    <div className="table__office">Nur-Sultan</div>
+                    <div className="table__office">
+                        <div className="table__time">23:05</div>
+                        <div className="table__open">hilversum</div>
+                    </div>
                     <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America 228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
                     <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
                 </div>
                 <div className="table__row">
-                    <div className="table__time">23:05</div>
-                    <div className="table__office">Astana</div>
+                    <div className="table__office">
+                        <div className="table__time">23:05</div>
+                        <div className="table__open">hilversum</div>
+                    </div>
                     <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
                     <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
                 </div>
                 <div className="table__row">
-                    <div className="table__time">23:05</div>
-                    <div className="table__office">Astana</div>
+                    <div className="table__office">
+                        <div className="table__time">23:05</div>
+                        <div className="table__open">hilversum</div>
+                    </div>
                     <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
                     <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
                 </div>
                 <div className="table__row">
-                    <div className="table__time">23:05</div>
-                    <div className="table__office">Astana</div>
+                    <div className="table__office">
+                        <div className="table__time">23:05</div>
+                        <div className="table__open">hilversum</div>
+                    </div>
+                    <div className="table__contact">Oude Amersfoortseweg 125 1212AA Hilversum The Netherlands</div>
+                    <div className="table__contact">hilversum@mediamonks.com +31 35 621 21 50</div>
+                </div>
+                <div className="table__row">
+                    <div className="table__office">
+                        <div className="table__time">23:05</div>
+                        <div className="table__open">hilversum</div>
+                    </div>
                     <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
                     <div className="table__contact">NY 10017 United States of America</div>
                 </div>
                 <div className="table__row">
-                    <div className="table__time">23:05</div>
-                    <div className="table__office">Nur-Sultan</div>
+                    <div className="table__office">
+                        <div className="table__time">23:05</div>
+                        <div className="table__open">hilversum</div>
+                    </div>
                     <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America 228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
                     <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
                 </div>
                 <div className="table__row">
-                    <div className="table__time">23:05</div>
-                    <div className="table__office">Astana</div>
+                    <div className="table__office">
+                        <div className="table__time">23:05</div>
+                        <div className="table__open">hilversum</div>
+                    </div>
                     <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
                     <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
                 </div>
                 <div className="table__row">
-                    <div className="table__time">23:05</div>
-                    <div className="table__office">Astana</div>
-                    <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
-                    <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
-                </div>
-                <div className="table__row">
-                    <div className="table__time">23:05</div>
-                    <div className="table__office">Astana</div>
-                    <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
-                    <div className="table__contact">NY 10017 United States of America</div>
-                </div>
-                <div className="table__row">
-                    <div className="table__time">23:05</div>
-                    <div className="table__office">Nur-Sultan</div>
-                    <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America 228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
-                    <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
-                </div>
-                <div className="table__row">
-                    <div className="table__time">23:05</div>
-                    <div className="table__office">Astana</div>
-                    <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
-                    <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
-                </div>
-                <div className="table__row">
-                    <div className="table__time">23:05</div>
-                    <div className="table__office">Astana</div>
+                    <div className="table__office">
+                        <div className="table__time">23:05</div>
+                        <div className="table__open">hilversum</div>
+                    </div>
                     <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
                     <div className="table__contact">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
                 </div>
