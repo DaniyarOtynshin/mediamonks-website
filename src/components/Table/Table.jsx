05252bb00d049891e7512f244ab20aec7a3eda9a
Daniyar Otynshin <daniyar.otynshin@yandex.kz>
2021-05-02 02:48:15 +0600

    Change styles for Table

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -5,25 +5,19 @@ const Table = () => {
                     <div className="table__title table__title_time">office time</div>
                     <div className="table__title table__title_open">office open</div>
                     <div className="table__title table__title_address">addres</div>
-                    <div className="table__title table__title_address">contact</div>
+                    <div className="table__title table__title_contact">contact</div>
                 </div>
                 <div className="table__row">
-                    <div className="table__time">23:05</div>
-                    <div className="table__open">hilversum</div>
-                    <div className="table__contact table__contact_address">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
+                    <div className="table__time _closed">23:05</div>
+                    <div className="table__open _closed">hilversum</div>
+                    <div className="table__contact table__contact_address"> 228 East 45th Street, Suite 9E New York, NY 10017 United States of America 228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
                     <div className="table__contact table__contact_contact">NY 10017 United States of America</div>
                 </div>
                 <div className="table__row">
-                    <div className="table__time">23:05</div>
-                    <div className="table__open">hilversum</div>
-                    <div className="table__contact table__contact_address">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
-                    <div className="table__contact table__contact_contact">NY 10017 United States of America</div>
-                </div>
-                <div className="table__row">
-                    <div className="table__time">23:05</div>
-                    <div className="table__open">hilversum</div>
-                    <div className="table__contact table__contact_address">228 East 45th Street, Suite 9E New York, NY 10017 United States of America</div>
-                    <div className="table__contact table__contact_contact">NY 10017 United States of America</div>
+                    <div className="table__time _closed">02:45</div>
+                    <div className="table__open _closed">nur-sultan</div>
+                    <div className="table__contact table__contact_address">010000 Nur-Sultan city Saryarka Avenue 6 Room 1 Office 1310 Kazakhstan</div>
+                    <div className="table__contact table__contact_contact">nursultan@mediamonks.com +7 (7172) 790389</div>
                 </div>
                 <div className="table__row">
                     <div className="table__time">23:05</div>
