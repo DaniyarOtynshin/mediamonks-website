1baeac55055f16ed04ee23bf7aba888f34de2f98
Daniyar Otynshin <daniyar.otynshin@yandex.kz>
2021-05-08 22:46:42 +0600

    Change Contact data

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -5,9 +5,11 @@ const ContactItem = (props) => {
             <div className="contact-item__data">
                 <p className="contact-item__description">{props.description}</p>
                 <p className="contact-item__address">{props.address}</p>
-                <a href="#" className="contact-item__mail">{props.mail}</a>
+                <a href={`mailto: ${props.mail}`} className="contact-item__contact-data">{props.mail}</a>
+                <a href={`tel: ${props.tel}`} className="contact-item__contact-data">{props.tel}</a>
                 <h4 className="contact-item__subtitle">contact</h4>
-                <a href="#" className="contact-item__data-info">{props.dataInfo}</a>
+                <a href={`mailto: ${props.dataInfo.mail}`} className="contact-item__data-info">{props.dataInfo.mail}</a>
+                <a href={`tel: ${props.dataInfo.tel}`} className="contact-item__data-info">{props.dataInfo.tel}</a>
             </div>
     </div>
     )
