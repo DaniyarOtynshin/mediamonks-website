9d0d13969e510101841de05a860358bc51603963
Daniyar Otynshin <daniyar.otynshin@yandex.kz>
2021-05-08 18:06:21 +0600

    Convert careers__filter-office in FilterOffice

--- src/components/Careers/Careers.jsx
+++ src/components/Careers/Careers.jsx
@@ -1,50 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
+import FilterOffice from '../FilterOffice/FilterOffice';
 import Vacancies from '../Vacancies/Vacancies';
 
-const offices = ["amsterdam", "berlin", "buenos aires", "dubai", "hilversum", "kuala lumpur", "london", "los angeles", "melbourne", "mexico city", "new delhi", "new york", "nur-sultan", "riyadh", "san francisco", "shanghai", "singapore", "sydney", "são paulo", "toronto"];
-
-
 const Careers = () => {
-
-    const [activeOffice, setActiveOffice] = useState('');
 
     useEffect(() => {
         window.scrollTo({top: 0});
     })
 
-    const onClick = (office) => {
-        setActiveOffice(office)
-    }
-
     return (
         <section className="careers">
             <h2 className="careers__title">careers</h2>
-            <div className="careers__filter-office-wrapper">
-                <ul className="careers__filter-office">
-                    <li className="careers__filter-office-item">all</li>
-                    {
-                        offices.map((office, index) => {
-                            return (
-                                <li
-                                key={index}
-                                onClick={() => onClick(office)}
-                                className={`careers__filter-office-item ${activeOffice === office ? "_active" : ""}`}
-                                >
-                                    {office}
-                                </li>
-                            )
-                        })
-                    }
-                    <li className="careers__filter-office-item careers__filter-office-item_button">
-                        <button className="block-button">
-                            <span className="block-text">
-                                show
-                                <span className="block-rest">all</span>
-                            </span>
-                        </button>
-                    </li>
-                </ul>
-            </div>
+            <FilterOffice />
             <Vacancies />
             <h2 className="careers__applications">open applications</h2>
             <div className="careers__open-open">
